55f7a9a2ae6046f6fa8a23fb620846707fc99ef5
Mathias Germandsen <math128w@edu.mercantec.dk>
2024-01-16 13:23:35 +0100

    random

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bf05d83d40d6cd6b430305fb5af02419cc5e968")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fa3315e6ffc2ff562e250f9cadd8d4da40150cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
