744dce9b02c43d3bf886a2556a1bb7140ed11719
Mathias Germandsen <math128w@edu.mercantec.dk>
2024-01-17 08:34:10 +0100

    Exercise - Implement the conditional operator

--- Part3/obj/Debug/net8.0/Part3.AssemblyInfo.cs
+++ Part3/obj/Debug/net8.0/Part3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Part3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29dfe4c3efeddff44e00a175928faefc1df6c34e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52979d3adfa9cd055d2a146853c1949842730921")]
 [assembly: System.Reflection.AssemblyProductAttribute("Part3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Part3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
