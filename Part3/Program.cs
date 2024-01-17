29dfe4c3efeddff44e00a175928faefc1df6c34e
Mathias Germandsen <math128w@edu.mercantec.dk>
2024-01-17 08:21:59 +0100

    outcommented

--- Part3/Program.cs
+++ Part3/Program.cs
@@ -1,4 +1,4 @@
-﻿string value1 = " a";
-string value2 = "A ";
+﻿// string value1 = " a";
+// string value2 = "A ";
 
-Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
