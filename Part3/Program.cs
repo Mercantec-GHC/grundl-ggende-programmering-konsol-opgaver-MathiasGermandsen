ff02601b11522789f34a9ca9b569dd9b66bb5fe9
Mathias Germandsen <math128w@edu.mercantec.dk>
2024-01-17 08:54:09 +0100

    Complete a challenge activity using conditional operators

--- Part3/Program.cs
+++ Part3/Program.cs
@@ -20,9 +20,20 @@
 // Console.WriteLine(pangram.Contains("fox"));
 // Console.WriteLine(pangram.Contains("cow"));
 
-int a = 7;
-int b = 6;
-Console.WriteLine(a != b); // output: True
-string s1 = "Hello";
-string s2 = "Hello";
-Console.WriteLine(s1 != s2); // output: False
+// int a = 7;
+// int b = 6;
+// Console.WriteLine(a != b); // output: True
+// string s1 = "Hello";
+// string s2 = "Hello";
+// Console.WriteLine(s1 != s2); // output: False
+
+// <evaluate this condition> ? <if condition is true, return this value> : <if condition is false, return this value>
+
+// int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {discount}");
+
+int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+
+Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
--- Part3/obj/Debug/net8.0/Part3.AssemblyInfo.cs
+++ Part3/obj/Debug/net8.0/Part3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Part3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52979d3adfa9cd055d2a146853c1949842730921")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744dce9b02c43d3bf886a2556a1bb7140ed11719")]
 [assembly: System.Reflection.AssemblyProductAttribute("Part3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Part3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
