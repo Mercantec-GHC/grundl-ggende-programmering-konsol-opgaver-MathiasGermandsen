42ad596529cabc04b03ef453106466f413675be9
Mathias Germandsen <math128w@edu.mercantec.dk>
2024-01-18 12:40:12 +0100

    fortune the cookie

--- part 5 methods/obj/Debug/net8.0/part 5 methods.AssemblyInfo.cs
+++ part 5 methods/obj/Debug/net8.0/part 5 methods.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("part 5 methods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cc5775f7161eef851a7b0fea795d6db99a15699")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b48e5d0112ca000467ff8ac8ce871482c9c6f142")]
 [assembly: System.Reflection.AssemblyProductAttribute("part 5 methods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("part 5 methods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
